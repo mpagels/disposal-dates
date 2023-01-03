6254a2c861f89cfba591fffe048eda6b718ac29c
Martin Pagels <office@martinpagels.de>
2023-01-04 00:29:12 +0100

    a backup file is saved, when recieving the dates

--- services/getData.py
+++ services/getData.py
@@ -1,10 +1,14 @@
-import requests
+import requests 
 
 def get_fallback_data():
-    stream = open('../calender.ics','rb')
+    stream = open('calender.ics','rb')
     ics = stream.read()
     stream.close()
     return ics
+
+def save_file_for_fallback_use(text, filename):
+    with open(filename, "w") as ics:
+        ics.write(text)
 
 def get_ics_data(dev=False):
     if dev:
@@ -12,6 +16,10 @@ def get_ics_data(dev=False):
     else:
         try:
             r = requests.get("https://www.geoport-nwm.de/nwm-download/Abfuhrtermine/ICS/2023/Ortsteil_Wahrsow.ics")
-            return r.text
+            if r.status_code == 200:
+                save_file_for_fallback_use(r.text, filename="calender.ics")
+                return r.text
+            elif r.status_code == 404:
+                return get_fallback_data()
         except:
             return get_fallback_data()
