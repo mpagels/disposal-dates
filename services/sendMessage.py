945aa8bb39658bf28ae38f1555c2793101dcf834
Martin Pagels <office@martinpagels.de>
2023-01-15 21:40:52 +0100

    sending message is now logged

--- services/sendMessage.py
+++ services/sendMessage.py
@@ -10,6 +10,7 @@ TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
 def send_message_via_WhatsApp(message):
     logging.info("trying send message")
+    logging.info(message)
     url = f"https://api.telegram.org/bot{TELEGRAM_KEY}/sendMessage?chat_id={TELEGRAM_CHAT_ID}&text={message}"
     requests.get(url).json()
     logging.info("send message")
